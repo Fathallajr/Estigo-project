134796cf9f7c2fe0b7c2f212d1ca8ecf684668ce
Mahmoud Fathallah <212103670@o6u.edu.eg>
2025-05-01 16:37:18 +0300

    prediction page

--- src/components/dashboard/prediction/prediction.component.ts
+++ src/components/dashboard/prediction/prediction.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+declare var CanvasJS: any;
+
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-prediction',
-  imports: [],
   templateUrl: './prediction.component.html',
-  styleUrl: './prediction.component.css'
+  styleUrls: ['./prediction.component.css']
 })
-export class PredictionComponent {
+export class PredictionComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.createChart();
+  }
+
+  private createChart(): void {
+    const chart = new CanvasJS.Chart("chartContainer", {
+      theme: "light2",
+      exportEnabled: false,
+      animationEnabled: true,
+      title: {
+        text: "EstiGo prediction"
+      },
+      data: [{
+        type: "pie",
+        startAngle: 25,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          { y: 50, label: "Attendance" },
+          { y: 20, label: "Avg quiz score" },
+         { y: 20, label: "Final Exam Score" },
+         { y: 10, label: "Student Exam Avg" },
+        ]
+      }]
+    });
+    chart.render();
+  }
 }
